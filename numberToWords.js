66e3edbf740fc8cb05b81390f442ac9f78067e8f
madhuripatil06 <madhuri@thoughtworks.com>
2016-11-18 12:24:11 +0530

    refactor num to word library

--- numberToWords.js
+++ numberToWords.js
@@ -1,54 +1,57 @@
 var oneDigitNumber = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen","Fifteen", "Sixteen", "Seventeen","eighteen","Nineteen","Twenty" ];
-var t = ["", "Ten","Twenty","Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "Twenty"];
-var positions = {"3":"Thousend", "6": "Million", "9": "Billion"};
+var ten = ["", "Ten","Twenty","Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "Twenty"];
+var positions = {3:"Thousend", 6: "Million", 9: "Billion"};
 
-
- var parse2Digitnumber = function(number){
-	if(+(number.join("")) == 0) return "";
+var parse2Digitnumber = function(number){
+	if(+(number) == 0) return "";
 	if(number[1] == 0)
-		return t[+number[0]];
-	if(+number > oneDigitNumber.length)
-		return t[+number[0]] + " "+  oneDigitNumber[+number[1]];
-	return oneDigitNumber[+(number[0]+number[1])];
+		return ten[+number[0]];
+	if(+number > oneDigitNumber.length){
+		return ten[+number[0]] + " "+  oneDigitNumber[+number[1]];
+	}
+	return oneDigitNumber[+(number)];
 };
 
 var parse3DigitNumber = function(number){
-	if(+(number.join("")) == 0) return "";
+	if(+(number) == 0) return "";
 	if(+(number[0])== 0 && +number[1] == 0)
 		return oneDigitNumber[+number[0]] +" Hundred";
-	return oneDigitNumber[+number[0]]+" Hundred " + t[(+number[1])] +" "+ oneDigitNumber[(+number[2])] ;
+	var result = oneDigitNumber[+number[0]]+" Hundred ";
+	result += parse2Digitnumber(number[1]+number[2])
+	return result;
 };
 
-var calculator = {};
-calculator["1"] = function(number){
-	return oneDigitNumber[+number[0]];
+var calculator = {
+	1: function(number){
+		return oneDigitNumber[+number[0]];
+	},
+	2: parse2Digitnumber,
+	3: parse3DigitNumber,
 };
-calculator["2"] = parse2Digitnumber;
-calculator["3"] = parse3DigitNumber;
 
-var splitBigNum = function(number){
+var splitBigNum = function(input){
 	var result = [];
-	var n = number.split("");
-	var copy = number.split("");
-	for(var i = 0 ; i < n.length/3 ; i++){
-		result.unshift(copy.splice(-3));
+	var number = input.split("");
+	var copy = input.split("");
+	for(var i = 0 ; i < number.length/3 ; i++){
+		result.unshift(copy.splice(-3).join(""));
 	};
-	if(copy.length > 0)
-		result.unshift(copy);
+	if(copy.length)
+		result.unshift(copy.join(""));
 	return parseBigNum(result);
 }
 
 var isItZero = function(number){
-	return (+number.join("")) == 0
+	return (+number) == 0
 }
 
 var parseBigNum = function(splitedNumber){
-	var result = ""
+	var result = "";
 	for(var i = 0 ; i < splitedNumber.length ; i++){
 		var position = positions[(splitedNumber.length - i-1)*3];
-		result += calculator[splitedNumber[i].length+""](splitedNumber[i])+ " ";
+		result += calculator[splitedNumber[i].length](splitedNumber[i])+ " ";
 		if(!isItZero(splitedNumber[i]) && position)
-			result += position + " " 
+			result += position + " "; 
 	}
 	return result;
 };
