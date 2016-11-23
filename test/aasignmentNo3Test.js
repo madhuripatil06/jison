32d1a562306976735c378ed21b3714bd9c2e9a10
madhuripatil06 <madhuri@thoughtworks.com>
2016-11-23 18:01:44 +0530

    refactor

--- test/aasignmentNo3Test.js
+++ test/aasignmentNo3Test.js
@@ -31,5 +31,16 @@ describe('Assignment No. 3 tests', function() {
 			var result = evaluator("x=1;y=7;x^y+9;");
 			assert.equal(10, result)
 		});
+
+		it('should return value for expression with complex power exp tree', function() {
+			var result = evaluator("x=2;x^4+9*6;");
+			assert.equal(70, result)
+		});
+
+
+		it('should return value for expression with complex exp tree', function() {
+			var result = evaluator("x=2;y=x;x=8;x+y;");
+			assert.equal(10, result)
+		});
 	});
 });
